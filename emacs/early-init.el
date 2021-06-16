dea7f965d478c6ddb66ec5efb5ecd1a7cac909d2
Ahnaf Al Rafi <ahnaf.al.rafi@gmail.com>
2021-06-16 03:54:19 -0500

    Barebones emacs early-init.el and init.el

---
+++ emacs/early-init.el
@@ -0,0 +1,29 @@
+;;; early-init.el --- -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+;; Defer loading package.el until called explicitly
+(setq package-enable-at-startup nil)
+
+;; Defer garbage collection
+(setq gc-cons-threshold most-positive-fixnum)
+
+;; Set file for custom.el to use
+(setq custom-file (concat user-emacs-directory "/custom.el"))
+
+(setq load-prefer-newer t)
+
+;; Disable gui elements before they are initialized.
+(push '(menu-bar-lines . 0) default-frame-alist)
+(setq menu-bar-mode nil)
+
+(push '(tool-bar-lines . 0) default-frame-alist)
+(setq tool-bar-mode nil)
+
+(push '(vertical-scroll-bars) default-frame-alist)
+(setq scroll-bar-mode nil)
+
+(provide 'early-init)
+;;; early-init.el ends here
---
+++ emacs/init.el
@@ -0,0 +1,76 @@
+;;; init.el --- -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+;; Personal details
+(setq user-full-name "Ahnaf Rafi")
+(setq user-mail-address "ahnaf.al.rafi@gmail.com")
+
+;; UTF-8
+(when (fboundp 'set-charset-priority)
+  (set-charset-priority 'unicode))
+(prefer-coding-system 'utf-8)
+(setq locale-coding-system 'utf-8)
+(setq selection-coding-system 'utf-8)
+
+;; Get rid of startup messages
+(setq inhibit-startup-message t)
+(setq inhibit-startup-echo-area-message user-login-name)
+(setq initial-scratch-message nil)
+(advice-add #'display-startup-echo-area-message :override #'ignore)
+
+;; yes/no to y/n
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; Cursor, tooltip and dialog box
+(when (fboundp 'blink-cursor-mode)
+  (blink-cursor-mode -1))
+(when (fboundp 'tooltip-mode)
+  (tooltip-mode -1))
+(setq visible-cursor nil)
+(setq use-dialog-box nil)
+(setq x-gtk-use-system-tooltips nil)
+
+;; Frame title: tell me if I am in daemon mode
+(setq frame-title-format (if (daemonp)
+                             '("AAR Daemacs - %b")
+                           '("AAR Emacs - %b")))
+
+;; Clipboard/kill-ring
+(setq kill-do-not-save-duplicates t)
+
+;; Auto-saves, backups and lockfiles
+(setq auto-save-default nil)
+(setq make-backup-files nil)
+(setq create-lockfiles nil)
+
+;; Scrolling
+(setq-default scroll-margin 0)
+(setq-default scroll-step 1)
+(setq-default scroll-preserve-screen-position nil)
+(setq-default scroll-conservatively 10000)
+(setq-default auto-window-vscroll nil)
+
+;; New lines at EOF
+(setq-default require-final-newline nil)
+(setq-default mode-require-final-newline t
+(setq-default log-edit-require-final-newline nil)
+
+;; Long lines
+(setq-default fill-column 80)
+(setq-default word-wrap t)
+(setq-default truncate-lines t)
+
+;; Indentation widths
+(setq-default standard-indent 2)
+(setq-default tab-width 2)
+(setq-default indent-tabs-mode nil)
+
+;; Favor vertical splits over horizontal ones. Screens are usually wide.
+(setq split-width-threshold 160)
+(setq split-height-threshold nil)
+
+(provide 'init)
+;;; init.el ends here
