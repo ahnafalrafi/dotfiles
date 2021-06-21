f78116b2fa21cd89dcc268ab682523cd75d25cbd
Ahnaf Al Rafi <ahnaf.al.rafi@gmail.com>
2021-06-20 21:59:26 -0500

    Rearrange things in LaTeX-mode config.
    Mainly, move function calls to single hook.

--- emacs/lisp/aar-latex.el
+++ emacs/lisp/aar-latex.el
@@ -4,52 +4,34 @@
 
 ;;; Code:
 
+;;; Built-in tex-mode - just a fallback, auctex is the goto
+;; This is just configuration for a fallback
+(setq tex-fontify-script nil)
+
 ;;; auctex
 (aar/maybe-install-package 'auctex)
+(add-to-list 'auto-mode-alist '("\\.tex\\'" . LaTeX-mode))
 
-(setq-default font-latex-fontify-script nil
-              font-latex-fontify-sectioning 'color
-              fill-nobreak-predicate nil
-              LaTeX-indent-environment-list nil
-              TeX-command-default "LatexMk"
-              TeX-electric-sub-and-superscript t
-              TeX-save-query nil
-              TeX-view-program-selection '((output-pdf "PDF Tools")
-                                           (output-pdf "Zathura"))
-              TeX-source-correlate-mode t
-              TeX-source-correlate-method 'synctex
-              TeX-source-correlate-start-server t
-              LaTeX-section-hook '(LaTeX-section-heading
-                                   LaTeX-section-title
-                                   LaTeX-section-toc
-                                   LaTeX-section-section
-                                   LaTeX-section-label)
-              LaTeX-fill-break-at-separators nil
-              LaTeX-item-indent 0)
-
-(with-eval-after-load 'eglot
-  (add-to-list 'eglot-server-programs
-             `((tex-mode context-mode texinfo-mode bibtex-mode) . ("texlab"))))
-
+(setq TeX-save-query nil)
+(setq TeX-view-program-selection '((output-pdf "PDF Tools")
+                                   (output-pdf "Zathura")))
+(setq TeX-source-correlate-mode t)
+(setq TeX-source-correlate-mode 'synctex)
+(setq TeX-source-correlate-start-server t)
+(setq TeX-electric-sub-and-superscript t)
+(setq LaTeX-indent-environment-list nil)
+(setq LaTeX-section-hook '(LaTeX-section-heading
+                           LaTeX-section-title
+                           LaTeX-section-toc
+                           LaTeX-section-section
+                           LaTeX-section-label))
+(setq LaTeX-fill-break-at-separators nil)
+(setq LaTeX-item-indent 0)
+(setq font-latex-fontify-script nil)
+(setq font-latex-fontify-sectioning 'color)
 (with-eval-after-load 'font-latex
   (set-face-foreground 'font-latex-script-char-face nil))
 
-(defun aar/LaTeX-mode-settings-h ()
-  (setq-local preivew-scale-function 1.25)
-  (setq-local spell-fu-faces-exclude '(font-latex-math-face
-                                       font-latex-sedate-face
-                                       font-latex-warning-face
-                                       font-lock-function-name-face
-                                       font-lock-keyword-face
-                                       font-lock-constant-face
-                                       font-lock-variable-name-face
-                                       button))
-  (visual-line-mode)
-  (auto-fill-mode)
-  (adaptive-wrap-prefix-mode)
-  (eglot-ensure))
-
-(add-hook 'LaTeX-mode-hook #'aar/LaTeX-mode-settings-h)
 (add-hook 'TeX-after-compilation-finished-functions-hook
           #'TeX-revert-document-buffer)
 
@@ -69,34 +51,56 @@
 ;;; auctex-latexmk
 (aar/maybe-install-package 'auctex-latexmk)
 (setq auctex-latexmk-inherit-TeX-PDF-mode t)
-
-;;;###autoload
-(defun aar/auctex-latexmk-h ()
-  (require 'auctex-latexmk)
-  (auctex-latexmk-setup))
-(add-hook 'LaTeX-mode-hook #'aar/auctex-latexmk-h)
+(setq TeX-command-default "LatexMk")
 
 ;;; evil-tex
 (aar/maybe-install-package 'evil-tex)
 (setq evil-tex-toggle-override-m nil)
 (setq evil-tex-toggle-override-t t)
 
-(add-hook 'LaTeX-mode-hook #'evil-tex-mode)
-
 ;;; reftex
 (setq reftex-plug-into-AUCTeX t)
 (setq reftex-toc-split-windows-fraction 0.3)
-(add-hook 'LaTeX-mode-hook #'reftex-mode)
 (add-hook 'reftex-mode-hook #'evil-normalize-keymaps)
 
 (define-key aar/localleader-LaTeX-mode-map (kbd ";") #'reftex-toc)
 
 ;;; bibtex
-(setq bibtex-dialect 'biblatex
-      bibtex-align-at-equal-sign t
-      bibtex-text-indentation 20)
+(setq bibtex-dialect 'biblatex)
+(setq bibtex-align-at-equal-sign t)
+(setq bibtex-text-indentation 20)
 (with-eval-after-load 'bibtex
   (define-key bibtex-mode-map (kbd "C-c \\") #'bibtex-fill-entry))
+
+;;; eglot configuration for latex
+(with-eval-after-load 'eglot
+  (add-to-list
+   'eglot-server-programs
+   `((tex-mode context-mode texinfo-mode bibtex-mode) . ("texlab"))))
+
+;;; Hook for tex modes
+(defun aar/latex-mode-h ()
+  (setq-local fill-nobreak-predicate nil)
+  (setq-local preivew-scale-function 1.25)
+  (setq-local spell-fu-faces-exclude '(font-lock-function-name-face
+                                       font-lock-keyword-face
+                                       font-lock-constant-face
+                                       font-lock-variable-name-face
+                                       font-latex-math-face
+                                       font-latex-sedate-face
+                                       font-latex-warning-face
+                                       button))
+
+  (visual-line-mode)
+  (auto-fill-mode)
+  (adaptive-wrap-prefix-mode)
+  (require 'auctex-latexmk)
+  (auctex-latexmk-setup)
+  (evil-tex-mode)
+  (reftex-mode)
+  (eglot-ensure))
+
+(add-hook 'LaTeX-mode-hook #'aar/latex-mode-h)
 
 (provide 'aar-latex)
 ;;; aar-latex.el ends here
