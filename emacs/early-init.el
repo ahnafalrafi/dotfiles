49314cd5fbff11cf963e927c79500b474b04a35f
Ahnaf Al Rafi <ahnaf.al.rafi@gmail.com>
2021-07-08 19:53:10 -0500

    Edits to: UI elements, no-littering, native comp.
    - UI elements: check existence of feature providing each early disabled UI
      element before disabling it
    - no-littering: Start own implementation of functionality provided by
      no-littering package (and start phasing out no-littering in process). This
      uses XDG Base Directory specification as well as the paradigm used by Doom
      Emacs within its doom-local-dir.
    - native-compilation: Move location of eln-cache to directories provided by own
      no-littering implementation

--- emacs/early-init.el
+++ emacs/early-init.el
@@ -13,14 +13,55 @@
 (setq load-prefer-newer t)
 
 ;; Disable gui elements before they are initialized.
-(push '(menu-bar-lines . 0) default-frame-alist)
-(setq menu-bar-mode nil)
+(when (featurep 'menu-bar)
+  (push '(menu-bar-lines . 0) default-frame-alist)
+  (setq menu-bar-mode nil))
 
-(push '(tool-bar-lines . 0) default-frame-alist)
-(setq tool-bar-mode nil)
+(when (featurep 'tool-bar)
+  (push '(tool-bar-lines . 0) default-frame-alist)
+  (setq tool-bar-mode nil))
 
-(push '(vertical-scroll-bars) default-frame-alist)
-(setq scroll-bar-mode nil)
+(when (featurep 'scroll-bar)
+  (push '(vertical-scroll-bars) default-frame-alist)
+  (setq scroll-bar-mode nil))
+
+;; Directories for keeping user-emacs-directory clean
+(defconst aar/etc-dir (expand-file-name
+                       (convert-standard-filename "emacs/")
+                       (or (getenv "XDG_DATA_HOME")
+                           (expand-file-name "~/.local/share/")))
+  "Directory for non-volatile local storage.
+
+Used for files that don't change much. Must end with a slash.")
+
+(defconst aar/cache-dir (expand-file-name
+                         (convert-standard-filename "emacs/")
+                         (or (getenv "XDG_CACHE_HOME")
+                             (expand-file-name "~/.cache/")))
+  "Directory for volatile local storage.
+
+Used for files that change often. Must end with a slash.")
+
+;; Make above directories if they don't exist
+(dolist (dir (list aar/etc-dir
+                   aar/cache-dir))
+  (unless (file-directory-p dir)
+    (make-directory dir)))
+
+(defun aar/expand-etc-file-name (file)
+  "Expand filename FILE relative to `aar/etc-dir'."
+  (expand-file-name (convert-standard-filename file) aar/cache-dir))
+
+(defun aar/expand-cache-file-name (file)
+  "Expand filename FILE relative to `aar/cache-dir'."
+  (expand-file-name (convert-standard-filename file) aar/cache-dir))
+
+(when (featurep 'native-compile)
+  ;; Silence compiler warnings as they can be pretty disruptive
+  (setq comp-async-report-warnings-errors nil)
+
+  ;; Set the right directory to store the native comp cache
+  (setcar native-comp-eln-load-path (aar/expand-cache-file-name "eln-cache/")))
 
 (provide 'early-init)
 ;;; early-init.el ends here
