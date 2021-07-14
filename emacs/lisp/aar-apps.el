f233fa27b4d60f14967a3b181ee5629aba53c275
Ahnaf Al Rafi <ahnaf.al.rafi@gmail.com>
2021-07-13 19:27:32 -0500

    Changes to ebib configuration.
    - Set ebib's default and search directory to ~/Dropbox/research/.
    - Preload ~/Dropbox/research/bibliography.bib using relative path.
    - Enable visual-line-mode in ebib-entry-mode
    - Add leader and localleader bindings to launch ebib as well as jumping to
      entries in ebib-index-mode.
    - Remap save-buffer to ebib-save-current-database in ebib-index-mode. This is
      what C-x C-s does in ebib-index-mode anyway.

--- emacs/lisp/aar-apps.el
+++ emacs/lisp/aar-apps.el
@@ -125,13 +125,37 @@ method to prepare vterm at the corresponding remote directory."
 
 ;;; ebib
 (aar/maybe-install-package 'ebib)
+(setq ebib-default-directory "~/Dropbox/research/")
 (setq ebib-bibtex-dialect 'biblatex)
+;; Open files in pdf-tools
 (setq ebib-file-associations nil)
+(setq ebib-bib-search-dirs '("~/Dropbox/research/"))
+(setq ebib-preload-bib-files '("bibliography.bib"))
 
 (defun aar/ebib-entry-mode-h ()
-  (setq-local show-trailing-whitespace nil))
+  (setq-local show-trailing-whitespace nil)
+  (visual-line-mode))
 
 (add-hook 'ebib-entry-mode-hook #'aar/ebib-entry-mode-h)
+
+;;;;;; Keybindings
+;; <leader> keybinding to launch ebib
+(define-key aar/leader-apps-map (kbd "b") #'ebib)
+
+;; <localleader> bindings for ebib-index-mode
+(define-prefix-command 'aar/localleader-ebib-index-mode-map)
+(evil-define-key '(normal visual motion) ebib-index-mode-map
+  (kbd aar/localleader-key) 'aar/localleader-ebib-index-mode-map)
+(evil-define-key '(insert emacs) ebib-index-mode-map
+  (kbd aar/localleader-alt-key) 'aar/localleader-ebib-index-mode-map)
+
+;; <localleader> jump to entry
+(define-key aar/localleader-ebib-index-mode-map (kbd "j") #'ebib-jump-to-entry)
+
+;; Remap save-buffer ebib-save-current-database in ebib-index-mode-map. This is
+;; what C-x C-s does in ebib-index-mode-map anyway.
+(define-key ebib-index-mode-map
+  [remap save-buffer] #'ebib-save-current-database)
 
 (provide 'aar-apps)
 ;;; aar-apps.el ends here
