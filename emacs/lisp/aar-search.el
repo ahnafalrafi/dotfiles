c6735e032f02c9576a5dd92fe6ec76c63f7358f4
Ahnaf Al Rafi <ahnaf.al.rafi@gmail.com>
2021-07-29 20:31:35 -0500

    Keybinding for consult-line

--- emacs/lisp/aar-search.el
+++ emacs/lisp/aar-search.el
@@ -34,6 +34,7 @@
 ;;; Keybindings
 (define-key aar/leader-search-map (kbd "c") #'evil-ex-nohighlight)
 (define-key aar/leader-search-map (kbd "p") #'consult-ripgrep)
+(define-key aar/leader-search-map (kbd "l") #'consult-line)
 
 (provide 'aar-search)
 ;;; aar-search.el ends here
