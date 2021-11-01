d5a280415d9c3d16b5c104d32478d0cdfee09355
Ahnaf Al Rafi <ahnaf.al.rafi@gmail.com>
2021-11-01 00:56:01 -0500

    Add FileIO package.

--- julia/setup_packages.jl
+++ julia/setup_packages.jl
@@ -1,14 +1,15 @@
 using Pkg
-Pkg.add(["NLopt", "GLPK", "JuMP", "Optim", "StaticArrays", "HDF5", "JLD", "CSV",
-         "DataFrames", "StatFiles", "ReadStat", "NPZ", "CategoricalArrays",
-         "ShiftedArrays", "StatsBase", "Distributions", "Distances",
-         "StatsFuns", "LogExpFunctions", "MultivariateStats", "GLM",
-         "MixedModels", "TimeSeries", "Clustering", "Loess", "KernelDensity",
-         "HypothesisTests", "Bootstrap", "StatsKit", "Lasso", "MLBase", "Plots",
-         "StatsPlots", "IJulia", "PackageCompiler", "BenchmarkTools",
-         "LanguageServer", "SymbolServer", "StaticLint", "Debugger",
-         "LibSSH2_jll", "JuliaInterpreter", "MKL_jll", "Grisu", "GR_jll",
-         "Latexify", "LibVPX_jll", "Contour", "PrettyTables", "EzXML"])
+Pkg.add(["NLopt", "GLPK", "JuMP", "Optim", "StaticArrays", "FileIO", "HDF5",
+         "JLD", "CSV", "DataFrames", "StatFiles", "ReadStat", "NPZ",
+         "CategoricalArrays", "ShiftedArrays", "StatsBase", "Distributions",
+         "Distances", "StatsFuns", "LogExpFunctions", "MultivariateStats",
+         "GLM", "MixedModels", "TimeSeries", "Clustering", "Loess",
+         "KernelDensity", "HypothesisTests", "Bootstrap", "StatsKit", "Lasso",
+         "MLBase", "Plots", "StatsPlots", "IJulia", "PackageCompiler",
+         "BenchmarkTools", "LanguageServer", "SymbolServer", "StaticLint",
+         "Debugger", "LibSSH2_jll", "JuliaInterpreter", "MKL_jll", "Grisu",
+         "GR_jll", "Latexify", "LibVPX_jll", "Contour", "PrettyTables",
+         "EzXML"])
 
 # Extra packages:
 # Pkg.add(["PenaltyFunctions", "ToeplitzMatrices", "LDLFactorizations"])
