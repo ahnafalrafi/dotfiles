733a402fd7e9e1a78125167aefa8c2370df08fc9
Ahnaf Al Rafi <ahnaf.al.rafi@gmail.com>
2021-11-04 22:33:57 -0500

    Move to straight, use doom-themes.

--- emacs/lisp/aar-visuals.el
+++ emacs/lisp/aar-visuals.el
@@ -20,81 +20,95 @@
 (setq use-dialog-box nil)
 (setq x-gtk-use-system-tooltips nil)
 
-;;; Display line numbers
+;;; Display line numbers and fill-column indicator
 (setq display-line-numbers-type 'visual)
-(add-hook 'prog-mode-hook #'display-line-numbers-mode)
-(add-hook 'text-mode-hook #'display-line-numbers-mode)
-
-;;; Display fill-column-indicator
-(add-hook 'prog-mode-hook #'display-fill-column-indicator-mode)
-(add-hook 'text-mode-hook #'display-fill-column-indicator-mode)
+(dolist (hook '(prog-mode-hook text-mode-hook))
+  (add-hook hook #'display-line-numbers-mode)
+  (add-hook hook #'display-fill-column-indicator-mode))
 
 ;;; Theme
-(defvar aar/modus-vivendi t
-  "Indicate whether modus-vivendi (the dark variant) should be loaded.")
+(straight-use-package 'doom-themes)
+(setq doom-themes-enable-bold t)
+(setq doom-themes-enable-italic t)
 
-(defun aar/load-modus-theme ()
-  "Load modus theme variant according to value of `aar-modus-vivendi'."
-  (setq modus-themes-no-mixed-fonts t)
-  (setq modus-themes-paren-match '(bold intense))
-  (setq modus-themes-org-blocks 'gray-background)
-  (if aar/modus-vivendi
-      (load-theme 'modus-vivendi t)
-    (load-theme 'modus-operandi t)))
+(defvar aar/theme 'doom-gruvbox)
 
-(defun aar/load-modus-theme-after-frame-h (frame)
-  "Load modus theme after the frame has been made.
-Useful for loading the themes properly in daemon mode"
+;;;###autoload
+(defun aar/load-theme ()
+  "Wrapper function around commands for loading theme."
+  ;; Global settings (defaults)
+  (setq doom-gruvbox-dark-variant "hard")
+  (load-theme 'doom-gruvbox t)
+  ;; (load-theme 'doom-dracula t)
+  ;; (set-face-attribute 'vertical-border nil
+  ;;                     :foreground "#6272a4"
+  ;;                     :background "#6272a4")
+  )
+
+;;;###autoload
+(defun aar/load-theme-after-frame-h (frame)
+  "Load theme after the frame has been made.
+Useful for loading themes properly in daemon mode"
   (with-selected-frame frame
-    (aar/load-modus-theme)))
+    (aar/load-theme)))
 
 (if (daemonp)
-    (add-hook 'after-make-frame-functions #'aar/load-modus-theme-after-frame-h)
-  (aar/load-modus-theme))
+    (add-hook 'after-make-frame-functions #'aar/load-theme-after-frame-h)
+  (aar/load-theme))
+
+;; Enable flashing mode-line on errors
+(doom-themes-visual-bell-config)
+;; Corrects (and improves) org-mode's native fontification.
+(doom-themes-org-config)
 
 ;;; Icons
-(aar/maybe-install-package 'all-the-icons)
-(require 'all-the-icons)
+(straight-use-package 'all-the-icons)
 
 ;;; Modeline
-;;;;;; Base modeline settings
+;; Base modeline settings
 (size-indication-mode t)
 (line-number-mode t)
 (column-number-mode t)
 
-;;;;;; hide-mode-line
-(aar/maybe-install-package 'hide-mode-line)
-(require 'hide-mode-line)
+;; doom-modeline
+(straight-use-package 'doom-modeline)
+(setq doom-modeline-height 12)
+(setq doom-modeline-bar-width 3)
+(setq doom-modeline-window-width-limit fill-column)
+(add-hook 'after-init-hook #'doom-modeline-mode)
+
+;; hide-mode-line
+(straight-use-package 'hide-mode-line)
 (add-hook 'completion-list-mode-hook #'hide-mode-line-mode)
 
 ;;; hl-todo: additional highlighting for TODO keywords
-(aar/maybe-install-package 'hl-todo)
+(straight-use-package 'hl-todo)
 (dolist (hook '(prog-mode-hook tex-mode-hook markdown-mode-hook))
   (add-hook hook #'hl-todo-mode))
 
 ;; Stolen from doom-emacs: modules/ui/hl-todo/config.el
-(setq hl-todo-highlight-punctuation ":"
-        hl-todo-keyword-faces
-        `(;; For things that need to be done, just not today.
-          ("TODO" warning bold)
-          ;; For problems that will become bigger problems later if not
-          ;; fixed ASAP.
-          ("FIXME" error bold)
-          ;; For tidbits that are unconventional and not intended uses of the
-          ;; constituent parts, and may break in a future update.
-          ("HACK" font-lock-constant-face bold)
-          ;; For things that were done hastily and/or hasn't been thoroughly
-          ;; tested. It may not even be necessary!
-          ("REVIEW" font-lock-keyword-face bold)
-          ;; For especially important gotchas with a given implementation,
-          ;; directed at another user other than the author.
-          ("NOTE" success bold)
-          ;; For things that just gotta go and will soon be gone.
-          ("DEPRECATED" font-lock-doc-face bold)
-          ;; For a known bug that needs a workaround
-          ("BUG" error bold)
-          ;; For warning about a problematic or misguiding code
-          ("XXX" font-lock-constant-face bold)))
+(setq hl-todo-highlight-punctuation ":")
+(setq  hl-todo-keyword-faces
+      `(;; For things that need to be done, just not today.
+        ("TODO" warning bold)
+        ;; For problems that will become bigger problems later if not
+        ;; fixed ASAP.
+        ("FIXME" error bold)
+        ;; For tidbits that are unconventional and not intended uses of the
+        ;; constituent parts, and may break in a future update.
+        ("HACK" font-lock-constant-face bold)
+        ;; For things that were done hastily and/or hasn't been thoroughly
+        ;; tested. It may not even be necessary!
+        ("REVIEW" font-lock-keyword-face bold)
+        ;; For especially important gotchas with a given implementation,
+        ;; directed at another user other than the author.
+        ("NOTE" success bold)
+        ;; For things that just gotta go and will soon be gone.
+        ("DEPRECATED" font-lock-doc-face bold)
+        ;; For a known bug that needs a workaround
+        ("BUG" error bold)
+        ;; For warning about a problematic or misguiding code
+        ("XXX" font-lock-constant-face bold)))
 
 (provide 'aar-visuals)
 ;;; aar-visuals.el ends here
