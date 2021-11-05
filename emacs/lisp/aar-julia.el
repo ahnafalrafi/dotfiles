cde52beab870049a0e3ea80c49bbb08850f6a647
Ahnaf Al Rafi <ahnaf.al.rafi@gmail.com>
2021-11-05 00:20:07 -0500

    Remove redundancies from ess-julia-mode code.

--- emacs/lisp/aar-julia.el
+++ emacs/lisp/aar-julia.el
@@ -14,14 +14,6 @@
 (evil-define-key '(insert emacs) julia-mode-map
   (kbd aar/localleader-alt-key) 'aar/localleader-julia-mode-map)
 
-(with-eval-after-load 'julia-mode
-  (evil-define-key '(normal visual motion) julia-mode-map
-    (kbd aar/localleader-key) 'aar/localleader-julia-mode-map)
-  (evil-define-key '(insert emacs) julia-mode-map
-    (kbd aar/localleader-alt-key) 'aar/localleader-julia-mode-map)
-
-  (define-key ess-julia-mode-map (kbd "TAB") #'julia-latexsub-or-indent))
-
 ;;; julia-repl
 (straight-use-package 'julia-repl)
 (define-key aar/localleader-julia-mode-map (kbd "r") #'julia-repl)
