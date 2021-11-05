60e6dc2682849a3a5f79254022d962448aa50b6f
Ahnaf Al Rafi <ahnaf.al.rafi@gmail.com>
2021-11-04 22:30:40 -0500

    Switch to straight.el, add autoload and adjust comments.

--- emacs/lisp/aar-keybindings.el
+++ emacs/lisp/aar-keybindings.el
@@ -5,19 +5,19 @@
 ;;; Code:
 
 ;;; which-key
-(aar/maybe-install-package 'which-key)
+(straight-use-package 'which-key)
 (setq which-key-idle-delay 0.3)
 (setq which-key-allow-evil-operators t)
 (which-key-mode)
 (which-key-setup-side-window-bottom)
 
 ;;; key-chord
-(aar/maybe-install-package 'key-chord)
+(straight-use-package 'key-chord)
 (require 'key-chord)
 (key-chord-mode 1)
 
 ;;; evil
-(aar/maybe-install-package 'evil)
+(straight-use-package 'evil)
 (setq evil-want-integration t)
 (setq evil-want-keybinding nil)
 (setq evil-want-C-u-scroll t)
@@ -39,37 +39,38 @@
 (evil-mode 1)
 (evil-set-initial-state 'messages-buffer-mode 'normal)
 
-;;;;;; evil-collection
-(aar/maybe-install-package 'evil-collection)
+;;; evil-collection
+(straight-use-package 'evil-collection)
 (setq evil-collection-outline-bind-tab-p nil)
 (setq evil-collection-want-unimpaired-p nil)
 (add-hook 'emacs-startup-hook #'evil-collection-init)
 
-;;;;;; evil-traces
-(aar/maybe-install-package 'evil-traces)
+;;; evil-traces
+(straight-use-package 'evil-traces)
 (require 'evil-traces)
 (evil-traces-use-diff-faces)
 (evil-traces-mode)
 
 ;;; Keybinding definitions
 
-;;;;;; Better escape
+;; Better escape
 (global-set-key (kbd "<escape>") #'keyboard-escape-quit)
 
-;;;;;; Text scale and zoom
+;; Text scale and zoom
 (global-set-key (kbd "C-+") #'text-scale-increase)
 (global-set-key (kbd "C-_") #'text-scale-decrease)
 (global-set-key (kbd "C-)") #'text-scale-adjust)
 
-;;;;;; Key chords for reverting to normal mode
+;; Key chords for reverting to normal mode
 (key-chord-define evil-insert-state-map  "fd" #'evil-normal-state)
 (key-chord-define evil-replace-state-map "fd" #'evil-normal-state)
 (key-chord-define evil-emacs-state-map   "fd" #'evil-normal-state)
 
-;;;;;; Universal arguments with evil
+;; Universal arguments with evil
 (global-set-key (kbd "C-M-u") 'universal-argument)
 
-;;;;;; Visual indent/dedent
+;; Visual indent/dedent
+;;;###autoload
 (defun aar/evil-visual-dedent ()
   "Equivalent to vnoremap < <gv."
   (interactive)
@@ -77,6 +78,7 @@
   (evil-normal-state)
   (evil-visual-restore))
 
+;;;###autoload
 (defun aar/evil-visual-indent ()
   "Equivalent to vnoremap > >gv."
   (interactive)
@@ -88,12 +90,14 @@
   (kbd "<") #'aar/evil-visual-dedent
   (kbd ">") #'aar/evil-visual-indent)
 
-;;;;;; Jumping to function and method definitions
+;; Jumping to function and method definitions
+;;;###autoload
 (defun aar/next-beginning-of-method (count)
   "Jump to the beginning of the COUNT-th method/function after point."
   (interactive "p")
   (beginning-of-defun (- count)))
 
+;;;###autoload
 (defun aar/previous-beginning-of-method (count)
   "Jump to the beginning of the COUNT-th method/function before point."
   (interactive "p")
@@ -102,6 +106,7 @@
 (defalias #'aar/next-end-of-method #'end-of-defun
   "Jump to the end of the COUNT-th method/function after point.")
 
+;;;###autoload
 (defun aar/previous-end-of-method (count)
   "Jump to the end of the COUNT-th method/function before point."
   (interactive "p")
@@ -113,8 +118,8 @@
   (kbd "] M") #'aar/next-end-of-method
   (kbd "[ M") #'aar/previous-end-of-method)
 
-;;;;;; <leader> and <localleader> implementations
-;;;;;;;;; Override mode and map to trigger <leader> keys
+;;; <leader> and <localleader> implementations
+;; Override mode and map to trigger <leader> keys
 (defvar aar/leader-override-mode-map (make-sparse-keymap)
   "Override Keymap to trigger \"leader key\" shortcuts.")
 
@@ -135,7 +140,7 @@
 ;; `minor-mode-map-alist'
 (add-to-list 'emulation-mode-map-alists 'aar/maps-alist)
 
-;;;;;;;;; <leader> key definitions, keymap and trigger bindings
+;; <leader> key definitions, keymap and trigger bindings
 (defconst aar/leader-key     "SPC")
 (defconst aar/leader-alt-key "M-SPC")
 (defconst aar/localleader-key     "SPC m")
@@ -153,7 +158,7 @@
 (evil-define-key '(insert emacs) 'aar/leader-override-mode-map
   (kbd aar/leader-alt-key) aar/leader-map)
 
-;;;;;; Some basic <leader> keybindings
+;; Some basic <leader> keybindings
 (define-key aar/leader-map (kbd ":") #'pp-eval-expression)
 (define-key aar/leader-map (kbd ";") #'execute-extended-command)
 (define-key aar/leader-map (kbd "&") #'async-shell-command)
