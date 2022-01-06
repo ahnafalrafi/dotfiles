50b6084f563b865a04d75560a6c13b93b96d4703
Ahnaf Al Rafi <ahnaf.al.rafi@gmail.com>
2022-01-06 15:57:37 -0600

    Use standard library directly rather than relying on LOAD_PATH.

--- julia/lang_server_invoke.jl
+++ julia/lang_server_invoke.jl
@@ -4,7 +4,7 @@ using LanguageServer
 using SymbolServer
 depot = get(ENV, "JULIA_DEPOT_PATH", "")
 project = dirname(something(Base.current_project(pwd()),
-                            Base.load_path_expand(LOAD_PATH[2])))
+                            Base.load_path_expand("@v#.#")))
 # Make sure that we only load packages from this environment specifically.
 @info "Running language server" env=Base.load_path()[1] pwd() project depot
 server = LanguageServer.LanguageServerInstance(stdin, stdout, project, depot)
