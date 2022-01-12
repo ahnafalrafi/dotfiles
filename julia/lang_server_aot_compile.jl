9f29ace3b45f5290e33acc7d6dd16c1145ef5ce7
Ahnaf Al Rafi <ahnaf.al.rafi@gmail.com>
2022-01-11 21:09:09 -0600

    Remove redundancies.

--- julia/lang_server_aot_compile.jl
+++
@@ -1,4 +0,0 @@
-using PackageCompiler
-create_sysimage([:LanguageServer, :SymbolServer, :StaticLint, :Debugger],
-                sysimage_path=string(Base.ENV["XDG_CACHE_HOME"],
-                                     "/julia/julials.so"))
