122bdbf393d97edd030e1b8cb841602252492128
Ahnaf Al Rafi <ahnaf.al.rafi@gmail.com>
2022-01-11 20:59:05 -0600

    Add custom.el.

---
+++ emacs/custom.el
@@ -0,0 +1,27 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(evil-ex-substitute-matches ((t (:inherit diff-removed :foreground unspecified :background unspecified))))
+ '(evil-ex-substitute-replacement ((t (:inherit diff-added :foreground unspecified :background unspecified))))
+ '(evil-traces-change ((t (:inherit diff-removed))))
+ '(evil-traces-copy-preview ((t (:inherit diff-added))))
+ '(evil-traces-copy-range ((t (:inherit diff-changed))))
+ '(evil-traces-delete ((t (:inherit diff-removed))))
+ '(evil-traces-global-match ((t (:inherit diff-added))))
+ '(evil-traces-global-range ((t (:inherit diff-changed))))
+ '(evil-traces-join-indicator ((t (:inherit diff-added))) t)
+ '(evil-traces-join-range ((t (:inherit diff-changed))))
+ '(evil-traces-move-preview ((t (:inherit diff-added))))
+ '(evil-traces-move-range ((t (:inherit diff-removed))))
+ '(evil-traces-normal ((t (:inherit diff-changed))))
+ '(evil-traces-shell-command ((t (:inherit diff-changed))))
+ '(evil-traces-substitute-range ((t (:inherit diff-changed))))
+ '(evil-traces-yank ((t (:inherit diff-changed)))))
